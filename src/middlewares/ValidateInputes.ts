661658c0b6733443b5dcf45d98d4aeb1a853250d
mitchel-okomor <mitchel.okomor@gmail.com>
2022-04-28 11:55:26 +0100

    extra time

--- src/middlewares/ValidateInputes.ts
+++ src/middlewares/ValidateInputes.ts
@@ -3,8 +3,8 @@ import {body as validBody, validationResult} from 'express-validator'
 
 
 const validate = (req, res, next)=>{
-	validBody('product_name').isLength({min: 6});
-	validBody('product_discription').isLength({min: 6})
+	validBody('product_name').isLength({min: 9});
+	validBody('product_discription').isLength({min: 9})
 
 	const errors = validationResult(req);
 	if(!errors.isEmpty()){
